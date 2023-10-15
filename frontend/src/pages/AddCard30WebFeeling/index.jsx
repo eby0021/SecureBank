d52a6bd86632453b69c932517d0d39c742f37daf
Ebin <c3421076@gmail.com>
2023-10-16 00:11:52 +0500

    card management page is ready to go

--- frontend/src/pages/AddCard30WebFeeling/index.jsx
+++ frontend/src/pages/AddCard30WebFeeling/index.jsx
@@ -105,7 +105,7 @@ const AddCard30WebFeelingPage = () => {
                   <div className="flex relative w-[70%] md:w-full">
                     <div className="h-[200px] sm:h-[490px] my-auto w-[90%] md:w-full">
                       <div className="absolute bg-light_blue-800 flex flex-col h-full inset-y-[0] items-center justify-start left-[3%] my-auto p-[15px] rounded-[30px] w-[91%]">
-                        <div className="flex flex-col justify-start mb-[37px] w-full">
+                        <div className="flex flex-col justify-start mb-[17px] w-full">
                           <div className="flex flex-row sm:gap-10 gap-[280px] items-start justify-end ml-12 md:ml-[0] w-[89%] md:w-full">
                             <Img
                               className="h-[60px] w-[200px]"
@@ -113,14 +113,16 @@ const AddCard30WebFeelingPage = () => {
                               alt="iconcard"
                             />
                             <Text
-                              className="backdrop-opacity-[0.5] blur-[10.00px] mt-[25px] sm:text-[21px] md:text-[23px] text-[25px] text-center text-white-A700 w-[27%] sm:w-full"
+                              className="backdrop-opacity-[0.5] blur-[10.00px] mr-[20px] mt-[5px] sm:text-[21px] md:text-[10px] text-[15px]
+                               text-center text-white-A700 w-[20%] sm:w-full"
                               size="txtPoppinsSemiBold25"
                             >
                               Visa Card
                             </Text>
                           </div>
                           <Text
-                            className="backdrop-opacity-[0.5] blur-[10.00px] md:ml-[0] ml-[23px] mt-[27px] sm:text-[21px] md:text-[23px] text-[25px] text-center text-white-A700 w-[44%] sm:w-full"
+                            className="backdrop-opacity-[0.5] blur-[10.00px] md:ml-[0]
+                             ml-[13px] mt-[0px] sm:text-[21px] md:text-[23px] text-[15px] text-center text-white-A700 w-[44%] sm:w-full"
                             size="txtPoppinsRegular25"
                           >
                             <span className="text-white-A700 font-poppins font-semibold">
@@ -133,9 +135,9 @@ const AddCard30WebFeelingPage = () => {
                               John Smith
                             </span>
                           </Text>
-                          <div className="flex sm:flex-col flex-row sm:gap-10 items-center justify-between mt-[142px] w-full">
+                          <div className="flex sm:flex-col flex-row sm:gap-10 items-center justify-between mt-[0px] w-full">
                             <Text
-                              className="backdrop-opacity-[0.5] blur-[10.00px] sm:text-[21px] md:text-[23px] text-[25px] text-center text-white-A700"
+                              className="backdrop-opacity-[0.5] blur-[10.00px] sm:text-[21px] md:text-[23px] text-[15px] text-center text-white-A700"
                               size="txtPoppinsMedium25"
                             >
                               7000 4000 5000 8000
@@ -241,71 +243,120 @@ const AddCard30WebFeelingPage = () => {
             </div>
             <Line className="bg-black-900_3f md:h-0.5 h-[1499px] md:ml-[0] ml-[149px] md:mt-0 mt-[42px] w-0.5 md:w-full" />
             <div className="bg-white-A700_01 border border-light_blue-800 border-solid flex md:flex-1 flex-col items-center justify-start mb-56 md:ml-[0] ml-[180px] md:mt-0 mt-[42px] p-[17px] rounded-[30px] shadow-bs5 w-[34%] md:w-full">
-              <div className="flex flex-col items-center justify-start mb-[19px] w-[98%] md:w-full">
+              <div className="flex flex-col items-center justify-start mb-[19px] w-[98%] md:w-full align-center">
                 <Img
                   className="h-[151px]"
                   src="images/img_iconcards.svg"
                   alt="iconcards"
                 />
                 <Text
-                  className="mt-[15px] sm:text-[37px] md:text-[43px] text-[47px] text-black-900"
+                  className="mt-[15px] sm:text-[37px] md:text-[43px] text-[35px] text-black-900 "
                   size="txtJostRomanBold47"
                 >
                   Activate a New Card
                 </Text>
-                <div className="bg-white-A700 border border-light_blue-900 border-solid flex sm:flex-col flex-row font-poppins gap-2.5 items-start justify-end mt-8 p-1 rounded-[30px] w-[58%] md:w-full">
+                <div className="bg-white-A700 border border-light_blue-900 border-solid flex sm:flex-col 
+                flex-row font-poppins gap-2.5 items-start justify-center mt-8 p-1 rounded-[30px] w-[48%] md:w-full align-center">
                 <select class="mb-4 sm:mt-0 mt-6 sm:text-[31px] md:text-[33px] text-[22px] text-gray-700 border-none " id="cardType">
-                <option value="card type" defaultChecked>Card Type</option>
                 <option value="credit">Credit</option>
-               <option value="debit">Debit</option>
-            </select>
-
-                
+               <option value="debit" defaultChecked>Debit</option>
+            </select>    
                 </div>
+
+
+
+
+
+
+
+
+
+
                 <div className="flex flex-col font-poppins items-start justify-start mt-[84px] w-[94%] md:w-full">
-                  <div className="bg-white-A700 border border-light_blue-900 border-solid flex sm:flex-col flex-row sm:gap-10 items-center justify-between p-0.5 rounded-tl-[10px] rounded-tr-[10px] w-full">
-                    <Text
-                      className="ml-8 sm:ml-[0] sm:text-[31px] md:text-[33px] text-[35px] text-gray-700"
+
+
+                  <div className="bg-white-A700 border border-light_blue-900 border-solid flex sm:flex-col
+                   flex-row sm:gap-10 items-center justify-between p-0.5 rounded-tl-[10px] rounded-tr-[10px] w-full">
+                    <input
+                      className="ml-8 sm:ml-[0] sm:text-[31px] md:text-[33px] text-[20px] text-gray-700"
                       size="txtPoppinsBold35"
-                    >
-                      Enter Card Holder Name
-                    </Text>
+                      placeholder="Enter Card Holder Name"
+                    />                     
                     <Img
-                      className="h-[74px] md:h-auto mb-[3px] sm:mt-0 mt-[9px] object-cover rounded-[10px]"
+                      className="h-[54px] md:h-auto mb-[3px] sm:mt-0 mt-[9px] object-cover rounded-[10px]"
                       src="images/img_keyboard8419852.png"
                       alt="keyboard8419852"
                     />
                   </div>
                   <Line className="bg-light_blue-900 h-[5px] mt-[5px] w-full" />
                   <div className="bg-white-A700 border border-light_blue-900 border-solid flex sm:flex-col flex-row sm:gap-10 items-center justify-between mt-16 pl-2 py-2 rounded-tl-[10px] rounded-tr-[10px] w-full">
-                    <Text
-                      className="sm:ml-[0] ml-[26px] sm:text-[31px] md:text-[33px] text-[35px] text-gray-700"
+                  <input
+                      className="ml-8 sm:ml-[0] sm:text-[31px] md:text-[33px] text-[20px] text-gray-700"
                       size="txtPoppinsBold35"
-                    >
-                      Enter Card Number
-                    </Text>
+                      placeholder="Enter Card Number"
+                    />     
                     <Img
-                      className="h-[74px] md:h-auto object-cover rounded-[10px]"
+                      className="h-[54px] md:h-auto object-cover rounded-[10px]"
                       src="images/img_keyboard8419852.png"
                       alt="keyboard8419852_One"
                     />
                   </div>
                   <Line className="bg-light_blue-900 h-[5px] mt-[5px] w-full" />
                   <div className="bg-white-A700 border border-light_blue-900 border-solid flex flex-row sm:gap-10 items-center justify-between mt-16 pl-[5px] py-[5px] rounded-tl-[10px] rounded-tr-[10px] w-full">
-                    <Text
-                      className="ml-[29px] sm:text-[31px] md:text-[33px] text-[35px] text-gray-700"
+                  <input
+                      className="ml-8 sm:ml-[0] sm:text-[31px] md:text-[33px] text-[20px] text-gray-700"
                       size="txtPoppinsBold35"
-                    >
-                      Enter CVC
-                    </Text>
+                      placeholder="Enter CVC"
+                    />     
                     <Img
-                      className="h-[74px] md:h-auto mt-1.5 object-cover rounded-[10px]"
+                      className="h-[54px] md:h-auto mt-1.5 object-cover rounded-[10px]"
                       src="images/img_keyboard8419852.png"
                       alt="keyboard8419852_Two"
                     />
                   </div>
                   <Line className="bg-light_blue-900 h-[5px] mt-[5px] w-full" />
-                  <div className="flex flex-row items-center justify-between md:ml-[0] ml-[45px] mt-[41px] w-[77%] md:w-full">
+
+
+
+
+                  <div className="bg-white-A700 border border-light_blue-900 border-solid flex sm:flex-col
+                   flex-row sm:gap-10 items-center justify-between p-0.5 rounded-tl-[10px] rounded-tr-[10px] w-full mt-[50px]">
+                    <input
+                      className="ml-8 sm:ml-[0] sm:text-[31px] md:text-[33px] text-[20px] text-gray-700"
+                      size="txtPoppinsBold35"
+                      placeholder="From month/year"
+                    />                     
+                    <Img
+                      className="h-[54px] md:h-auto mb-[3px] sm:mt-0 mt-[9px] object-cover rounded-[10px]"
+                      src="images/img_keyboard8419852.png"
+                      alt="keyboard8419852"
+                    />
+                  </div>
+                  <Line className="bg-light_blue-900 h-[5px] mt-[5px] w-full" />
+
+
+
+                  <div className="bg-white-A700 border border-light_blue-900 border-solid flex sm:flex-col
+                   flex-row sm:gap-10 items-center justify-between p-0.5 rounded-tl-[10px] rounded-tr-[10px] w-full mt-[50px]">
+                    <input
+                      className="ml-8 sm:ml-[0] sm:text-[31px] md:text-[33px] text-[20px] text-gray-700"
+                      size="txtPoppinsBold35"
+                      placeholder="To month/year"
+                    />                     
+                    <Img
+                      className="h-[54px] md:h-auto mb-[3px] sm:mt-0 mt-[9px] object-cover rounded-[10px]"
+                      src="images/img_keyboard8419852.png"
+                      alt="keyboard8419852"
+                    />
+                  </div>
+                  <Line className="bg-light_blue-900 h-[5px] mt-[5px] w-full" />
+
+
+
+
+
+
+                  {/* <div className="flex flex-row items-center justify-between md:ml-[0] ml-[45px] mt-[41px] w-[77%] md:w-full">
                     <Text
                       className="sm:text-[31px] md:text-[33px] text-[35px] text-gray-800"
                       size="txtPoppinsBold35Gray800"
@@ -318,45 +369,54 @@ const AddCard30WebFeelingPage = () => {
                     >
                       TO
                     </Text>
-                  </div>
+                  </div> */}
                 </div>
+
+
+
+
+
                 <div className="flex flex-col font-poppins items-center justify-start mt-[3px] w-full">
-                  <div className="flex md:flex-col flex-row gap-[19px] items-center justify-between w-full">
-                    <div className="bg-white-A700 border border-light_blue-900 border-solid flex md:flex-1 flex-row gap-8 items-center justify-between p-2 rounded-tl-[10px] rounded-tr-[10px] w-[51%] md:w-full">
-                      <Text
-                        className="ml-[5px] my-2.5 sm:text-[31px] md:text-[33px] text-[35px] text-gray-700"
-                        size="txtPoppinsBold35"
-                      >
-                        month/year
-                      </Text>
+                  {/* <div className="flex md:flex-col flex-row gap-[19px] items-center justify-between w-full">
+                    <div className="bg-white-A700 border border-light_blue-900 border-solid 
+                    flex md:flex-1 flex-row gap-8 items-center justify-start p-2 rounded-tl-[10px] rounded-tr-[10px] w-[51%] md:w-full">
+                    <input
+                      className="ml-8 sm:ml-[0] sm:text-[31px] md:text-[33px] text-[10px] text-gray-700 w-[80%]"
+                      size="txtPoppinsBold35"
+                      placeholder="month/year"
+                    />  
                       <Img
                         className="h-[46px] md:h-auto object-cover rounded-[10px]"
                         src="images/img_keyboard8419852.png"
                         alt="keyboard8419852_Three"
                       />
                     </div>
-                    <div className="bg-white-A700 border border-light_blue-900 border-solid flex md:flex-1 flex-row gap-[11px] items-center justify-between p-[9px] rounded-tl-[10px] rounded-tr-[10px] w-[48%] md:w-full">
-                      <Text
-                        className="my-[9px] sm:text-[31px] md:text-[33px] text-[35px] text-gray-700"
-                        size="txtPoppinsBold35"
-                      >
-                        month/year
-                      </Text>
+                    <div className="bg-white-A700 border border-light_blue-900 
+                    border-solid flex md:flex-1 flex-row gap-[11px] items-center justify-start p-[9px] rounded-tl-[10px] rounded-tr-[10px] w-[48%] md:w-full">
+                    <input
+                      className="ml-8 sm:ml-[0] sm:text-[31px] md:text-[33px] text-[20px] text-gray-700"
+                      size="txtPoppinsBold35"
+                      placeholder="month/year"
+                    />  
                       <Img
                         className="h-[51px] md:h-auto object-cover rounded-[10px]"
                         src="images/img_keyboard8419852.png"
                         alt="keyboard8419852_Four"
                       />
                     </div>
-                  </div>
-                  <div className="flex sm:flex-col flex-row gap-[19px] items-center justify-between mt-[5px] w-full">
+                  </div> */}
+                  {/* <div className="flex sm:flex-col flex-row gap-[19px] items-center justify-between mt-[5px] w-full">
                     <Line className="bg-light_blue-900 h-[5px] w-[51%]" />
                     <Line className="bg-light_blue-900 h-[5px] w-[48%]" />
-                  </div>
+                  </div> */}
                   <Button
-                    className="cursor-pointer font-semibold leading-[normal] min-w-[319px] mt-[59px] sm:text-[35px] md:text-[41px] text-[45px] text-center"
+                    className="cursor-pointer font-semibold leading-[normal] size-[100px] min-w-[19px] mt-[59px] sm:text-[35px] md:text-[41px] text-[25px] text-center"
                     shape="round"
-                    size="xs"
+                    onClick={()=>{
+                      alert('card verification API will send data to backend')
+                      
+                    }}
+                    
                   >
                     Verify Card
                   </Button>
